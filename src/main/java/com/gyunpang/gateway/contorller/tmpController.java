bc5fa887db7f0fd4507ebaa5b8c66bd5a01e3a7f
sok5188 <sok5188@gmail.com>
2024-03-24 20:05:29 +0900

    add color info

--- src/main/java/com/gyunpang/gateway/contorller/tmpController.java
+++ src/main/java/com/gyunpang/gateway/contorller/tmpController.java
@@ -1,11 +1,16 @@
 package com.gyunpang.gateway.contorller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class tmpController {
+
+	@Value("${container.color}")
+	private String colorInfo;
 	@RequestMapping("/health")
 	public ResponseEntity<String> healthCheck(){
 		return ResponseEntity.ok("alive");
