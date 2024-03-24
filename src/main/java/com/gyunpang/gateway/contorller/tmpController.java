49c836b5f7abb1e4a7e03737f5fb9119dd8e6f08
sok5188 <sok5188@gmail.com>
2024-03-24 21:05:25 +0900

    add log

--- src/main/java/com/gyunpang/gateway/contorller/tmpController.java
+++ src/main/java/com/gyunpang/gateway/contorller/tmpController.java
@@ -7,19 +7,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class tmpController {
 
 	@Autowired
 	private Environment environment;
 	@RequestMapping("/health")
 	public ResponseEntity<String> healthCheck(){
+		log.info("got health check");
 		return ResponseEntity.ok("alive");
 	}
 
 	@RequestMapping("/color")
 	public ResponseEntity<String> colorCheck(){
+		log.info("got color check");
 		if(environment.containsProperty("CONTAINER_COLOR")){
 			String customEnv = environment.getProperty("CONTAINER_COLOR");
 			return ResponseEntity.ok(customEnv);
