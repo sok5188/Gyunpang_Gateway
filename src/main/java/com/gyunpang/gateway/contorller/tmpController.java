bf3def1a985ca16b36938ce7d06835b9d64f63af
sok5188 <sok5188@gmail.com>
2024-09-29 16:02:12 +0900

    Rerun action

--- src/main/java/com/gyunpang/gateway/contorller/tmpController.java
+++ src/main/java/com/gyunpang/gateway/contorller/tmpController.java
@@ -14,19 +14,21 @@ import lombok.extern.slf4j.Slf4j;
 public class tmpController {
 
 	private final Environment environment;
+
 	@RequestMapping("/healthCheck")
-	public ResponseEntity<String> healthCheck(){
+	public ResponseEntity<String> healthCheck() {
 		log.info("got health check");
+
 		return ResponseEntity.ok("gateway alive");
 	}
 
 	@RequestMapping("/color")
-	public ResponseEntity<String> colorCheck(){
+	public ResponseEntity<String> colorCheck() {
 		log.info("got color check");
-		if(environment.containsProperty("CONTAINER_COLOR")){
+		if (environment.containsProperty("CONTAINER_COLOR")) {
 			String customEnv = environment.getProperty("CONTAINER_COLOR");
 			return ResponseEntity.ok(customEnv);
-		}
-		else return ResponseEntity.ok("NoCOLOR");
+		} else
+			return ResponseEntity.ok("NoCOLOR");
 	}
 }
